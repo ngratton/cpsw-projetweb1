cba644956dc16eb73034761d30e733aa71ee5c84
Nicholas Gratton <nichgratton@gmail.com>
2019-10-22 13:40:08 -0400

    First commit

---
+++ src/js.js
@@ -0,0 +1,28 @@
+/*jslint esnext:true, browser:true*/
+/**
+ * @module App
+ */
+export default class App {
+	/**
+	 * Méthode principale. Sera typiquement appelée après le chargement de la page.
+	 */
+	static main() {
+        var lienlogo = document.querySelector("#nav-logo");
+
+        lienlogo.addEventListener("click", e => {
+            window.location.href = './';
+        });
+    }
+
+	/**
+	 * Méthode qui permet d'attendre le chargement de la page avant d'éxécuter le script principal
+	 * @returns {Promise} La promesse qui sera résolue après chargement
+	 */
+	static load() {
+		return new Promise(resolve => {
+			window.addEventListener("load", () => {
+				resolve();
+			});
+		});
+	}
+}
